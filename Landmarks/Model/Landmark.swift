70671dd341ebc6eb28801ad0b6e84ec57830e7f1
nawta <39507181+nawta@users.noreply.github.com>
2023-04-12 21:45:52 +0900

    Section 7 Pass Data into Child Views ここまでmacOS開発してたのでiOSにBaseSDKを変更した

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
 
 struct Landmark: Hashable, Codable, Identifiable {
@@ -15,9 +16,21 @@ struct Landmark: Hashable, Codable, Identifiable {
     var park: String
     var state: String
     var description: String
-    
+
     private var imageName: String
     var image: Image {
         Image(imageName)
+    }
+
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
     }
 }
