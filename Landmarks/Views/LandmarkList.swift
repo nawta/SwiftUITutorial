0d9be9bf08ec9e4c995c2401de87280995e9e9f7
nawta <39507181+nawta@users.noreply.github.com>
2023-04-12 20:54:12 +0900

    Section 5 Make the List Dynamic

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List {
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
         }
     }
 }
@@ -21,3 +20,5 @@ struct LandmarkList_Previews: PreviewProvider {
         LandmarkList()
     }
 }
+
+
